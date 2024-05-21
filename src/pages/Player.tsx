a6a231f8b1bd6b5a7c2c04f6089a048723a6c097
Lucas Lira <lucasrocha446@gmail.com>
2024-05-20 21:43:28 -0300

    feat: add player reducer

--- src/pages/Player.tsx
+++ src/pages/Player.tsx
@@ -2,8 +2,11 @@ import { MessageCircle } from "lucide-react";
 import { Header } from "../components/Header";
 import { Video } from "../components/Video";
 import { Module } from "../components/Module";
+import { useAppSelector } from "../store";
 
 export function Player() {
+  const modules = useAppSelector((state) => state.player.course.modules);
+
   return (
     <div className="h-screen bg-zinc-950 text-zinc-50 flex justify-center items-center">
       <div className="flex w-[1100px] flex-col gap-6">
@@ -21,21 +24,14 @@ export function Player() {
             <Video />
           </div>
           <aside className="w-80 absolute top-0 bottom-0 right-0 border-l border-zinc-800 divide-y-2 divide-zinc-900 bg-zinc-900 overflow-y-scroll scrollbar scrollbar-thin scrollbar-track-zinc-950 scrollbar-thumb-zinc-800">
-            <Module
-              moduleIndex={0}
-              title="Fundamentos do Redux"
-              amountOfLessons={3}
-            />
-            <Module
-              moduleIndex={1}
-              title="Fundamentos do Redux"
-              amountOfLessons={3}
-            />
-            <Module
-              moduleIndex={2}
-              title="Fundamentos do Redux"
-              amountOfLessons={3}
-            />
+            {modules.map((module, index) => (
+              <Module
+                key={module.id}
+                moduleIndex={index}
+                title={module.title}
+                amountOfLessons={module.lessons.length}
+              />
+            ))}
           </aside>
         </main>
       </div>
